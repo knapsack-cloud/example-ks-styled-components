53629fb5a7c8da08e6b9f9b44d08ccaa9e660085
Christopher Bloom <bloomcb@gmail.com>
2023-12-07 07:02:11 -0800

    Apply prettier

--- packages/core-knapsack/demo-wrapper.tsx
+++ packages/core-knapsack/demo-wrapper.tsx
@@ -1,10 +1,10 @@
-import React, { PropsWithChildren } from "react";
-import { ThemeProvider, DefaultTheme } from "styled-components";
+import React, { PropsWithChildren } from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
 
-import GskDark from "@my-org/core-tokens/dist/GSK-Theme-dark/design-tokens.nested.json";
-import GskLight from "@my-org/core-tokens/dist/GSK-Theme-light/design-tokens.nested.json";
-import NucalaDark from "@my-org/core-tokens/dist/Nucala-Theme-dark/design-tokens.nested.json";
-import NucalaLight from "@my-org/core-tokens/dist/Nucala-Theme-light/design-tokens.nested.json";
+import GskDark from '@my-org/core-tokens/dist/GSK-Theme-dark/design-tokens.nested.json';
+import GskLight from '@my-org/core-tokens/dist/GSK-Theme-light/design-tokens.nested.json';
+import NucalaDark from '@my-org/core-tokens/dist/Nucala-Theme-dark/design-tokens.nested.json';
+import NucalaLight from '@my-org/core-tokens/dist/Nucala-Theme-light/design-tokens.nested.json';
 
 const themeLookup = {
   gsk: {
@@ -26,36 +26,25 @@ function isModeName(modeName: string): modeName is ModeNames {
   return modeName in themeLookup.gsk;
 }
 
-const brandName = document.body.getAttribute("data-brand");
-const modeName = document.body.getAttribute("data-mode");
+const brandName = document.body.getAttribute('data-brand');
+const modeName = document.body.getAttribute('data-mode');
 
 if (!brandName || !isBrandName(brandName)) {
   throw new Error(
     `Brand name "${brandName}" is not valid. Must be one of: ${Object.keys(
-      themeLookup,
-    ).join(", ")}`,
+      themeLookup
+    ).join(', ')}`
   );
 }
 if (!modeName || !isModeName(modeName)) {
   throw new Error(
     `Mode name "${modeName}" is not valid. Must be one of: ${Object.keys(
-      themeLookup.gsk,
-    ).join(", ")}`,
+      themeLookup.gsk
+    ).join(', ')}`
   );
 }
 
 const theme: DefaultTheme = themeLookup[brandName][modeName];
-
-// const theme: DefaultTheme = {
-//   color: {
-//     primary: {
-//       "01": "#000000",
-//     },
-//     base: {
-//       white: "#ffffff",
-//     },
-//   },
-// };
 
 export default ({ children }: PropsWithChildren<{}>) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
--- packages/core-knapsack/tokens.ts
+++ packages/core-knapsack/tokens.ts
@@ -1,18 +1,18 @@
-import { readFileSync, mkdirSync, writeFileSync, read } from "fs";
-import { join } from "path";
-import { execSync } from "child_process";
-import merge from "deepmerge";
+import { readFileSync, mkdirSync, writeFileSync, read } from 'fs';
+import { join } from 'path';
+import { execSync } from 'child_process';
+import merge from 'deepmerge';
 
 // The "sets" within Tokens Studio we want to combine
 const combos = [
-  ["GSK", "Theme-dark"],
-  ["GSK", "Theme-light"],
-  ["Nucala", "Theme-dark"],
-  ["Nucala", "Theme-light"],
+  ['GSK', 'Theme-dark'],
+  ['GSK', 'Theme-light'],
+  ['Nucala', 'Theme-dark'],
+  ['Nucala', 'Theme-light'],
 ];
 
 // Where we're going to dump everything
-const distFolder = join(__dirname, "../core-tokens/dist");
+const distFolder = join(__dirname, '../core-tokens/dist');
 // Make sure it exists first
 mkdirSync(distFolder, { recursive: true });
 
@@ -20,7 +20,7 @@ mkdirSync(distFolder, { recursive: true });
 function doTokensFiles(filePath: string) {
   // Read the file synchronously
   try {
-    const jsonData = JSON.parse(readFileSync(filePath, "utf8"));
+    const jsonData = JSON.parse(readFileSync(filePath, 'utf8'));
 
     // For every set we need to combine
     combos.forEach((combo) => {
@@ -29,7 +29,7 @@ function doTokensFiles(filePath: string) {
       // Smash all together, deep
       const merged = merge.all(mergeableObjects);
       // Using the keys within the object, create a file name
-      const tokenComboName = combo.map((key) => key).join("-");
+      const tokenComboName = combo.map((key) => key).join('-');
       const srcTokenFilePath = join(distFolder, `${tokenComboName}.json`);
       // Write out Tokens Studio JSON
       writeFileSync(srcTokenFilePath, JSON.stringify(merged, null, 2));
@@ -38,13 +38,13 @@ function doTokensFiles(filePath: string) {
       const assetsFolder = join(distFolder, tokenComboName);
       execSync(
         `npx knapsack-build-tokens --src ${srcTokenFilePath} --dist ${assetsFolder} --src-format tokens-studio`,
-        { stdio: "inherit" },
+        { stdio: 'inherit' }
       );
     });
   } catch (error) {
-    console.error("Error reading the file:", error);
+    console.error('Error reading the file:', error);
   }
 }
 
 // Replace 'path/to/your/jsonfile.json' with the actual file path
-doTokensFiles(join(__dirname, "../core-tokens/src/tokens-studio.json"));
+doTokensFiles(join(__dirname, '../core-tokens/src/tokens-studio.json'));
